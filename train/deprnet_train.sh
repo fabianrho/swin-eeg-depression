849f3bf63b6a2ad5f51f5b0ee1810e00baed8811
Fabian Hoitsma <30692009+fabianrho@users.noreply.github.com>
2024-06-23 17:29:32 +0200

    Add files via upload

---
+++ train/deprnet_train.sh
@@ -0,0 +1,9 @@
+python train_cv.py --model_type deprnet --timewindow 4 --overlap 0
+python train_cv.py --model_type deprnet --timewindow 4 --overlap 0.25
+python train_cv.py --model_type deprnet --timewindow 4 --overlap 0.5
+python train_cv.py --model_type deprnet --timewindow 4 --overlap 0.75
+
+python train_cv.py --model_type deprnet --timewindow 6 --overlap 0
+python train_cv.py --model_type deprnet --timewindow 6 --overlap 0.25
+python train_cv.py --model_type deprnet --timewindow 6 --overlap 0.5
+python train_cv.py --model_type deprnet --timewindow 6 --overlap 0.75
---
+++ train/swin_train.sh
@@ -0,0 +1,11 @@
+python train_cv.py --model_type swin --timewindow 4 --overlap 0
+python train_cv.py --model_type swin --timewindow 4 --overlap 0.25
+python train_cv.py --model_type swin --timewindow 4 --overlap 0.5
+python train_cv.py --model_type swin --timewindow 4 --overlap 0.75
+
+python train_cv.py --model_type swin --timewindow 6 --overlap 0
+python train_cv.py --model_type swin --timewindow 6 --overlap 0.25
+python train_cv.py --model_type swin --timewindow 6 --overlap 0.5
+python train_cv.py --model_type swin --timewindow 6 --overlap 0.75
+
+
